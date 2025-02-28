a04c97cd8b5b7f67c14874e60f795a33991b96d4
Abhay <abhaycormourya@gmail.com>
2025-02-28 22:24:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ let imageCounts = { body: 5, eyes: 7, hair: 64, cloths: 64 };
 let currentIndexes = { body: 0, eyes: 0, hair: 0, cloths: 0 };
 
 function changeLayer(layer, direction) {
-    let maxImages = imageCounts[layer]; // Get total images for the layer
+    let maxImages = imageCounts[layer];
     currentIndexes[layer] = (currentIndexes[layer] + direction + maxImages) % maxImages;
-    
+    updateImage(layer);
+}
+
+function randomizeAvatar() {
+    for (let layer in imageCounts) {
+        currentIndexes[layer] = Math.floor(Math.random() * imageCounts[layer]);
+        updateImage(layer);
+    }
+}
+
+function updateImage(layer) {
     let newImagePath = `${layer}/${currentIndexes[layer]}.png`;
     document.getElementById(layer).src = newImagePath;
 }
