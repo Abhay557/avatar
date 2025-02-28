6236bb1123160110ac2147d28dfe6997fcaa87b8
Abhay <abhaycormourya@gmail.com>
2025-02-28 16:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let layers = {};
-let currentIndexes = { layer1: 0, layer2: 0, layer3: 0, layer4: 0 };
+let imageCounts = { body: 5, eyes: 7, hair: 64, cloths: 64 };
+let currentIndexes = { body: 0, eyes: 0, hair: 0, cloths: 0 };
 
-// Load JSON data
-fetch('layers.json')
-    .then(response => response.json())
-    .then(data => {
-        layers = data.layers;
-    })
-    .catch(error => console.error('Error loading JSON:', error));
-
-// Function to change avatar layers
 function changeLayer(layer, direction) {
-    if (!layers[layer]) return; // Ensure layers are loaded
-    let images = layers[layer];
-    currentIndexes[layer] = (currentIndexes[layer] + direction + images.length) % images.length;
-    document.getElementById(layer).src = images[currentIndexes[layer]];
+    let maxImages = imageCounts[layer]; // Get total images for the layer
+    currentIndexes[layer] = (currentIndexes[layer] + direction + maxImages) % maxImages;
+    
+    let newImagePath = `${layer}/${currentIndexes[layer]}.png`;
+    document.getElementById(layer).src = newImagePath;
 }
